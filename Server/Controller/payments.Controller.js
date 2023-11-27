761b7d2af2d3ee55344fc17a3d62121b1b5bad04
Mousa-Ahmed0 <mousa.progm@gmail.com>
2023-11-27 18:05:05 +0200

    payment

--- Server/Controller/payments.Controller.js
+++ Server/Controller/payments.Controller.js
@@ -8,7 +8,6 @@ const { models } = require("mongoose");
  * @method POST
  * @access private (staff or admin ) 
  * -----------------------------------*/
-
 module.exports.addPayment = asyncHandler(async (req, res) => {
     //vaildition @front end 
     const newPayment = new payments({
@@ -19,16 +18,67 @@ module.exports.addPayment = asyncHandler(async (req, res) => {
         value: req.body.value,
         discountedValue: req.body.discountedValue,
     });
-    console.log(newPayment.value);
-    console.log(newPayment.InsuranceCompPers / 100);//pers
-    console.log(newPayment.value * (newPayment.InsuranceCompPers / 100));//discountedValue
-    console.log(newPayment.value - (newPayment.value * (newPayment.InsuranceCompPers / 100)));//value
-
     if (newPayment.InsuranceCompPers) {
-        const pers = newPayment.InsuranceCompPers / 100;
-        newPayment.discountedValue = newPayment.value * pers;
-        newPayment.value = newPayment.value - (newPayment.value * pers);
+        const pers = newPayment.InsuranceCompPers / 100;//pers
+        newPayment.discountedValue = newPayment.value * pers;//discountedValue
+        newPayment.value = newPayment.value - (newPayment.value * pers);//value
     }
     await newPayment.save();
     res.status(201).json({ newPayment, message: "done....." });
-})
+});
+/**-----------------------------------
+ * @desc get all  payments 
+ * @router /api/payment/getPayment
+ * @method GET 
+ * @access private (staff or admin ) 
+ * -----------------------------------*/
+module.exports.getPayment = asyncHandler(async (req, res) => { 
+    //vaildition @front end 
+    // const getAllPayment = await payments.find().select("value -_id ");
+    const getAllPayment = await payments.find();
+    if (getAllPayment){
+        let count=0;
+        for(let i=0;i<getAllPayment.length;i++){
+            count+=getAllPayment[i].value;
+        }
+        res.status(201).json({ count,getAllPayment, message: "done....." });
+    }
+    else
+        res.status(400).json({message:"Can't find repoet"});
+});
+
+
+/**-----------------------------------
+ * @desc get count payments 
+ * @router /api/payment/countPayment
+ * @method GET 
+ * @access private (staff or admin ) 
+ * -----------------------------------*/
+module.exports.countPayment = asyncHandler(async (req, res) => { 
+    //vaildition @front end 
+    const count = await payments.count();
+    if (count)
+        res.status(201).json({ count, message: "done....." });
+    else
+        res.status(400).json({message:"Can't find repoet"});
+});
+
+/**-----------------------------------
+ * @desc get payments  by identPatient
+ * @router /api/payment/getPaymentIdentPatient
+ * @method GET 
+ * @access private (staff or admin ) 
+ * -----------------------------------*/
+module.exports.getPaymentIdentPatient = asyncHandler(async (req, res) => { 
+    //vaildition @front end 
+    const getPayment = await payments.find({identPatient:req.body.identPatient});
+    if (getPayment){
+        let count=0;
+        for(let i=0;i<getPayment.length;i++){
+            count+=getPayment[i].value;
+        }
+        res.status(201).json({count, getPayment, message: "done....." });
+    }
+    else
+        res.status(400).json({message:"Can't find repoet"});
+});
--- Server/routes/payment.routes.js
+++ Server/routes/payment.routes.js
@@ -1,6 +1,9 @@
 const router=require("express").Router();
-const { addPayment } = require("../Controller/payments.Controller");
+const { addPayment, getPayment, countPayment, getPaymentIdentPatient } = require("../Controller/payments.Controller");
 const validateObjectId = require("../middlewares/validateObjectId");
 const { verifyToken, ifAdminOrStaff } = require("../middlewares/verifyToken");
 router.post('/addPayment',ifAdminOrStaff,addPayment);
+router.get('/getPayment',ifAdminOrStaff,getPayment);
+router.get('/countPayment',ifAdminOrStaff,countPayment);
+router.get('/getPaymentIdentPatient',ifAdminOrStaff,getPaymentIdentPatient);
 module.exports=router;
