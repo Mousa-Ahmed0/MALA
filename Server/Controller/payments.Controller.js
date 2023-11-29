0101f362ac18cb4e85b199b91d6d220bafbf9f35
Mousa-Ahmed0 <mousa.progm@gmail.com>
2023-11-29 19:35:39 +0200

    payment api

--- Server/Controller/payments.Controller.js
+++ Server/Controller/payments.Controller.js
@@ -1,12 +1,13 @@
 const asyncHandler = require("express-async-handler");
 const { payments } = require("../models/payments");
-const { models } = require("mongoose");
 
 /**-----------------------------------
  * @desc add payments
  * @router /api/payment/addPayment
  * @method POST
  * @access private (staff or admin )
+ * year/month/day
+ * 2023/09/05
  * -----------------------------------*/
 module.exports.addPayment = asyncHandler(async (req, res) => {
   //vaildition @front end
@@ -31,6 +32,8 @@ module.exports.addPayment = asyncHandler(async (req, res) => {
  * @router /api/payment/getPayment
  * @method GET
  * @access private (staff or admin )
+ *  year/month/day
+ * 2023/09/05
  * -----------------------------------*/
 module.exports.getPayment = asyncHandler(async (req, res) => {
   //vaildition @front end
@@ -49,6 +52,8 @@ module.exports.getPayment = asyncHandler(async (req, res) => {
  * @desc get count payments
  * @router /api/payment/countPayment
  * @method GET
+ * year/month/day
+ * 2023/09/05
  * @access private (staff or admin )
  * -----------------------------------*/
 module.exports.countPayment = asyncHandler(async (req, res) => {
@@ -62,6 +67,8 @@ module.exports.countPayment = asyncHandler(async (req, res) => {
  * @desc get payments  by identPatient
  * @router /api/payment/getPaymentIdentPatient
  * @method GET
+ *  year/month/day
+ * 2023/09/05
  * @access private (staff or admin )
  * -----------------------------------*/
 module.exports.getPaymentIdentPatient = asyncHandler(async (req, res) => {
@@ -76,4 +83,76 @@ module.exports.getPaymentIdentPatient = asyncHandler(async (req, res) => {
     }
     res.status(201).json({ count, getPayment, message: "done....." });
   } else res.status(400).json({ message: "Can't find repoet" });
+});
+
+/**-----------------------------------
+ * @desc get payments  by identPatient
+ * @router /api/payment/getByDate
+ * @method GET 
+ * year/month/day
+ * 2023/09/05
+ * @access private (staff or admin )
+ * -----------------------------------*/
+module.exports.getByDate = asyncHandler(async (req, res) => {
+  //vaildition @front end
+  const paymentDate = new Date(req.body.payDate);
+  const getPayment = await payments.find({ payDate: paymentDate });
+  console.log(getPayment)
+  if (getPayment) {
+    let count = 0;
+    for (let i = 0; i < getPayment.length; i++) {
+      count += getPayment[i].value;
+    }
+    res.status(201).json({ count, getPayment, message: "done....." });
+  } else res.status(400).json({ message: "Can't find repoet" });
+});
+
+/**-----------------------------------
+ * @desc get payments  by identPatient
+ * @router /api/payment/getFromToDate
+ * @method GET
+ * year/month/day
+ * 2023/09/05
+ * @access private (staff or admin )
+ * -----------------------------------*/
+module.exports.getFromToDate = asyncHandler(async (req, res) => {
+  //vaildition @front end
+  const firstDate = new Date(req.body.firstDate);
+  const secondtDate = new Date(req.body.secondtDate);
+  const getPayment = await payments.find({
+    payDate: { $gte: firstDate, $lte: secondtDate }
+  });
+  if (getPayment) {
+    let count = 0;
+    for (let i = 0; i < getPayment.length; i++) {
+      count += getPayment[i].value;
+    }
+    res.status(201).json({ count, getPayment, message: "done....." });
+  } else res.status(400).json({ message: "Can't find repoet" });
+});
+/**-----------------------------------
+ * @desc get payments  by identPatient
+ * @router /api/payment/getByDate
+ * @method GET
+ * year/month/day
+ * 2023/09/05
+ * @access private (staff or admin )
+ * -----------------------------------*/
+module.exports.test = asyncHandler(async (req, res) => {
+  //vaildition @front end
+  const paymentDate = new Date(req.body.payDate);
+  console.log(paymentDate.getDate());
+  console.log(paymentDate.getMonth() + 1);
+  console.log(paymentDate.getFullYear() );
+  // const getPayment = await payments.find({ payDate: paymentDate });
+  // console.log(getPayment)
+  // if (getPayment) {
+  //   let count = 0;
+  //   for (let i = 0; i < getPayment.length; i++) {
+  //     count += getPayment[i].value;
+  //   }
+  //   res.status(201).json({ count, getPayment, message: "done....." });
+  // } else res.status(400).json({ message: "Can't find repoet" });
+  res.status(400).json({ message: "Can't find repoet" });
+
 });
--- Server/routes/payment.routes.js
+++ Server/routes/payment.routes.js
@@ -1,9 +1,11 @@
 const router=require("express").Router();
-const { addPayment, getPayment, countPayment, getPaymentIdentPatient } = require("../Controller/payments.Controller");
+const { addPayment, getPayment, countPayment, getPaymentIdentPatient, getByDate, getFromToDate, test } = require("../Controller/payments.Controller");
 const validateObjectId = require("../middlewares/validateObjectId");
 const { verifyToken, ifAdminOrStaff } = require("../middlewares/verifyToken");
 router.post('/addPayment',ifAdminOrStaff,addPayment);
 router.get('/getPayment',ifAdminOrStaff,getPayment);
 router.get('/countPayment',ifAdminOrStaff,countPayment);
 router.get('/getPaymentIdentPatient',ifAdminOrStaff,getPaymentIdentPatient);
+router.get('/getByDate',ifAdminOrStaff,getByDate);
+router.get('/getFromToDate',ifAdminOrStaff,getFromToDate);
 module.exports=router;
