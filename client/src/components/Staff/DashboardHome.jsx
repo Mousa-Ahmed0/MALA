9e5b8204d72e661861b188b9180ca48f1de6864a
OmarKH24 <omark.nasser24@gmail.com>
2023-12-17 22:25:26 +0200

    Front: Admin Dashboard, users counts

--- client/src/components/Staff/DashboardHome.jsx
+++ client/src/components/Staff/DashboardHome.jsx
@@ -4,10 +4,64 @@ import { useDarkMode } from "../../context/DarkModeContext";
 import AdsSection from "../UserComponenet/Ads/AdsSection";
 import PaymentLineChartContainer from "./Charts/PaymentLineChart/PaymentLineChartContainer";
 import MessageBox from "./MessagesHandale/MessageBox/MessageBox";
+import axios from "axios";
+import { getDatasetAtEvent } from "react-chartjs-2";
 export default function DashboardHome({ user }) {
   const { darkMode } = useDarkMode();
   const width = "-webkit-fill-available";
-  useEffect(() => {}, []);
+  const [staffCount, setStaffCount] = useState(0);
+  const [patientCount, setPatientCount] = useState(0);
+  const [doctorsCount, setDoctorsCount] = useState(0);
+
+  //get Patients count
+  async function getDoctorsCount() {
+    try {
+      const response = await axios.get(
+        "http://localhost:5000/api/user/countDoctor",
+        {
+          headers: { Authorization: "Bearer " + localStorage.getItem("token") },
+        }
+      );
+      setDoctorsCount(response.data);
+    } catch (error) {
+      console.error("Error From getDoctorsCount: ", error);
+    }
+  }
+  //get Patients count
+  async function getStaffCount() {
+    try {
+      const response = await axios.get(
+        "http://localhost:5000/api/user/countStaff",
+        {
+          headers: { Authorization: "Bearer " + localStorage.getItem("token") },
+        }
+      );
+      setStaffCount(response.data);
+    } catch (error) {
+      console.error("Error From getStaffCount: ", error);
+    }
+  }
+  //get Patients count
+  async function getPatientCount() {
+    try {
+      const response = await axios.get(
+        "http://localhost:5000/api/user/countPatient",
+        {
+          headers: { Authorization: "Bearer " + localStorage.getItem("token") },
+        }
+      );
+      setPatientCount(response.data);
+    } catch (error) {
+      console.error("Error From Get PatientsNo.: ", error);
+    }
+  }
+  ///////////////////////////////
+  useEffect(() => {
+    //initial
+    getPatientCount();
+    getStaffCount();
+    getDoctorsCount();
+  }, []);
   return (
     <div className="ST-section ST-Dashboard">
       <DashboardWelcome user={user} />
@@ -23,7 +77,9 @@ export default function DashboardHome({ user }) {
               <i class="h3 colorMain fa-solid fa-globe"></i>
               <span className="h5 colorMain m-0">Users</span>
             </div>
-            <span className="h5 mid-bold">{38}</span>
+            <span className="h5 mid-bold">
+              {patientCount + doctorsCount + staffCount}
+            </span>
             <div style={{ width }} className="progress">
               <div
                 className="progress-bar w-100"
@@ -43,13 +99,21 @@ export default function DashboardHome({ user }) {
               <i class="h3 colorMain fa-solid fa-user"></i>
               <span className="h5 colorMain m-0">Staff</span>
             </div>
-            <span className="h5 mid-bold">{3}</span>
+            <span className="h5 mid-bold">{staffCount}</span>
             <div style={{ width }} className="progress">
               <div
                 className={`progress-bar`}
-                style={{ width: `${(3 * 100) / 38}%` }}
+                style={{
+                  width: `${
+                    (staffCount * 100) /
+                    (patientCount + doctorsCount + staffCount)
+                  }%`,
+                }}
                 role="progressbar"
-                aria-valuenow={(3 * 100) / 38}
+                aria-valuenow={
+                  (staffCount * 100) /
+                  (patientCount + doctorsCount + staffCount)
+                }
                 aria-valuemin="0"
                 aria-valuemax="100"
               ></div>
@@ -64,13 +128,21 @@ export default function DashboardHome({ user }) {
               <i class="h3 colorMain fa-solid fa-hospital-user"></i>
               <span className="h5 colorMain m-0">Patients</span>
             </div>
-            <span className="h5 mid-bold">{27}</span>
+            <span className="h5 mid-bold">{patientCount}</span>
             <div style={{ width }} className="progress">
               <div
                 className={`progress-bar`}
-                style={{ width: `${(27 * 100) / 38}%` }}
+                style={{
+                  width: `${
+                    (patientCount * 100) /
+                    (patientCount + doctorsCount + staffCount)
+                  }%`,
+                }}
                 role="progressbar"
-                aria-valuenow={(27 * 100) / 38}
+                aria-valuenow={
+                  (patientCount * 100) /
+                  (patientCount + doctorsCount + staffCount)
+                }
                 aria-valuemin="0"
                 aria-valuemax="100"
               ></div>
@@ -85,13 +157,21 @@ export default function DashboardHome({ user }) {
               <i class="h3 colorMain fa-solid fa-user-doctor"></i>
               <span className="h5 colorMain m-0">Doctors</span>
             </div>
-            <span className="h5 mid-bold">{8}</span>
+            <span className="h5 mid-bold">{doctorsCount}</span>
             <div style={{ width }} className="progress">
               <div
                 className={`progress-bar`}
-                style={{ width: `${(8 * 100) / 38}%` }}
+                style={{
+                  width: `${
+                    (doctorsCount * 100) /
+                    (patientCount + doctorsCount + staffCount)
+                  }%`,
+                }}
                 role="progressbar"
-                aria-valuenow={(8 * 100) / 38}
+                aria-valuenow={
+                  (doctorsCount * 100) /
+                  (patientCount + doctorsCount + staffCount)
+                }
                 aria-valuemin="0"
                 aria-valuemax="100"
               ></div>
