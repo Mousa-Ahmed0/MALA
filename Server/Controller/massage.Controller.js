9d3a6703971c0648bec922dbb301241ff2a29c67
Mousa-Ahmed0 <mousa.progm@gmail.com>
2023-12-18 18:15:57 +0200

    edit if ready

--- Server/Controller/massage.Controller.js
+++ Server/Controller/massage.Controller.js
@@ -7,47 +7,47 @@ const { Massage } = require("../models/message");
  * @access public
  * ------------------------------------------ */
 module.exports.sendMass = asyncHandler(async (req, res) => {
-  //if Patient determain admin _id
-  let objectIdString = "";
-  console.log(req.user.usertype);
-  if (req.user.usertype === "Patient")
-    objectIdString = process.env.ADMIN_ID; //admin _id
-  else objectIdString = req.body.recvId;
+    //if Patient determain admin _id
+    let objectIdString = "";
+    if (req.user.usertype === "Patient")
+        objectIdString = process.env.ADMIN_ID; //admin _id
+    else objectIdString = req.body.recvId;
 
-  let massRecord = await Massage.findOne({
-    $or: [
-      { senderId: req.user.id, recvId: objectIdString },
-      { senderId: objectIdString, recvId: req.user.id },
-    ],
-  });
+    let massRecord = await Massage.findOne({
+        $or: [
+            { senderId: req.user.id, recvId: objectIdString },
+            { senderId: objectIdString, recvId: req.user.id },
+        ],
+    });
 
-  if (massRecord) {
-    // Massage record exists, update it
-    massRecord.massage.push({
-      senderId: req.user.id,
-      mass: req.body.massage,
-      date: new Date(),
-    });
-    massRecord.ifReady = false;
-    await massRecord.save();
-    return res.status(200).json(massRecord);
-  } else {
-    // First massage
-    const newMass = new Massage({
-      senderId: req.user.id,
-      recvId: objectIdString,
-      massage: [
-        {
-          senderId: req.user.id,
-          mass: req.body.massage,
-          date: new Date(),
-        },
-      ],
-      ifReady: false,
-    });
-    await newMass.save();
-    return res.status(200).json(newMass);
-  }
+    if (massRecord) {
+        // Massage record exists, update it
+        massRecord.massage.push({
+            senderId: req.user.id,
+            mass: req.body.massage,
+            date: new Date(),
+        });
+        if (!(req.user.usertype === "Patient"))
+            massRecord.ifReady = false;
+        await massRecord.save();
+        return res.status(200).json(massRecord);
+    } else {
+        // First massage
+        const newMass = new Massage({
+            senderId: req.user.id,
+            recvId: objectIdString,
+            massage: [
+                {
+                    senderId: req.user.id,
+                    mass: req.body.massage,
+                    date: new Date(),
+                },
+            ],
+            ifReady: false,
+        });
+        await newMass.save();
+        return res.status(200).json(newMass);
+    }
 });
 /**--------------------------------
  * @desc get all Massage
@@ -56,13 +56,13 @@ module.exports.sendMass = asyncHandler(async (req, res) => {
  * @access public
  * ------------------------------------------ */
 module.exports.getAllMass = asyncHandler(async (req, res) => {
-  const newMass = await Massage.find({})
-    .populate("senderId", ["-password"])
-    .sort({ createdAt: 1 });
-  //.populate('recvId', ['-password'])
+    const newMass = await Massage.find({})
+        .populate("senderId", ["-password"])
+        .sort({ createdAt: 1 });
+    //.populate('recvId', ['-password'])
 
-  if (newMass) return res.status(200).json(newMass);
-  else return res.status(400).json({ massage: "Massage dose not exist" });
+    if (newMass) return res.status(200).json(newMass);
+    else return res.status(400).json({ massage: "Massage dose not exist" });
 });
 
 /**--------------------------------
@@ -72,12 +72,12 @@ module.exports.getAllMass = asyncHandler(async (req, res) => {
  * @access public
  * ------------------------------------------ */
 module.exports.getMass = asyncHandler(async (req, res) => {
-  const newMass = await Massage.find({ senderId: req.user.id })
-    .populate("senderId", ["-password"])
-    .sort({ createdAt: 1 });
-  //.populate('recvId', ['-password'])
-  if (newMass) return res.status(200).json(newMass);
-  else return res.status(400).json({ massage: "Massage dose not exist" });
+    const newMass = await Massage.find({ senderId: req.user.id })
+        .populate("senderId", ["-password"])
+        .sort({ createdAt: 1 });
+    //.populate('recvId', ['-password'])
+    if (newMass) return res.status(200).json(newMass);
+    else return res.status(400).json({ massage: "Massage dose not exist" });
 });
 /**--------------------------------
  * @desc get Massage
@@ -86,12 +86,12 @@ module.exports.getMass = asyncHandler(async (req, res) => {
  * @access public
  * ------------------------------------------ */
 module.exports.getUserMass = asyncHandler(async (req, res) => {
-  const newMass = await Massage.findById(req.params.id)
-    .populate("senderId", ["-password"])
-    .sort({ createdAt: 1 });
-  //.populate('recvId', ['-password'])
-  if (newMass) return res.status(200).json(newMass);
-  else return res.status(400).json({ massage: "Massage dose not exist" });
+    const newMass = await Massage.findById(req.params.id)
+        .populate("senderId", ["-password"])
+        .sort({ createdAt: 1 });
+    //.populate('recvId', ['-password'])
+    if (newMass) return res.status(200).json(newMass);
+    else return res.status(400).json({ massage: "Massage dose not exist" });
 });
 
 /**--------------------------------
@@ -101,9 +101,9 @@ module.exports.getUserMass = asyncHandler(async (req, res) => {
  * @access public
  * ------------------------------------------ */
 module.exports.deleteMass = asyncHandler(async (req, res) => {
-  const newMass = await Massage.findByIdAndDelete(req.params.id);
-  if (!newMass) return res.status(404).json({ message: "Massage not found" });
-  else return res.status(200).json({ message: "Massage is delete..." });
+    const newMass = await Massage.findByIdAndDelete(req.params.id);
+    if (!newMass) return res.status(404).json({ message: "Massage not found" });
+    else return res.status(200).json({ message: "Massage is delete..." });
 });
 
 /**--------------------------------
@@ -113,12 +113,12 @@ module.exports.deleteMass = asyncHandler(async (req, res) => {
  * @access public
  * ------------------------------------------ */
 module.exports.countIfRead = asyncHandler(async (req, res) => {
-  const newMass = await Massage.find({ ifReady: false }).count();
-  if (!newMass)
-    return res
-      .status(404)
-      .json({ message: "All masage is ready", count: newMass });
-  else return res.status(200).json({ No: newMass });
+    const newMass = await Massage.find({ ifReady: false }).count();
+    if (!newMass)
+        return res
+            .status(404)
+            .json({ message: "All masage is ready", count: newMass });
+    else return res.status(200).json({ No: newMass });
 });
 /**--------------------------------
  * @desc Edit if ready
@@ -127,13 +127,13 @@ module.exports.countIfRead = asyncHandler(async (req, res) => {
  * @access public
  * ------------------------------------------ */
 module.exports.editIfReady = asyncHandler(async (req, res) => {
-  const edit = await Massage.findByIdAndUpdate(
-    req.params.id,
-    {
-      ifReady: true,
-    },
-    { new: true }
-  );
-  if (edit) return res.status(200).json({ edit: "true" });
-  else return res.status(400).json({ edit: "False" });
+    const edit = await Massage.findByIdAndUpdate(
+        req.params.id,
+        {
+            ifReady: true,
+        },
+        { new: true }
+    );
+    if (edit) return res.status(200).json({ edit: "true" });
+    else return res.status(400).json({ edit: "False" });
 });
