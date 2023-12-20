09ee2e6372597d071622beab9e796c2660596fa2
OmarKH24 <omark.nasser24@gmail.com>
2023-12-20 07:46:42 +0200

    Result Table Updated

--- client/src/components/ResultDetails/ResultDetailsComponents/ResultsTable.jsx
+++ client/src/components/ResultDetails/ResultDetailsComponents/ResultsTable.jsx
@@ -1,12 +1,47 @@
 import React, { useEffect } from "react";
+import { formatDateWithouHour } from "../../../methods/FormateDate";
 export default function ResultsTable({ darkMode, resultDetails }) {
+  function renderNoOfResults(length, r) {
+    const arrayResults = r.reverse();
+    const results = [];
+    for (let i = 0; i < length; i++) {
+      if (i === 0) {
+        results.push(
+          <th className="result " key={i}>
+            New Result
+          </th>
+        );
+      } else
+        results.push(
+          <th className="result" key={i + 1}>
+            Previous Result{" "}
+            <span>({formatDateWithouHour(r[i].resultDate)})</span>
+          </th>
+        );
+    }
+    return results;
+  }
+
   function renderComponentsResult(anlyze, noOfAnlyze) {
-    return anlyze.result.map((comp, index) => {
-      console.log("Comp", comp);
+    // console.log("no:", resultDetails.analysArray[noOfAnlyze]);
+    //  console.log("anlyze", anlyze);
+    const compResult = anlyze.result[0].compontResult;
+    //console.log("compResult", compResult);
+
+    return compResult[0].resultValues.map((comp, index) => {
       return (
         <tr key={index}>
-          <td>{comp.name}</td>
-          <td>{comp.value}</td>
+          <td className="comp-name">
+            {resultDetails.analysArray[noOfAnlyze].compnents[index].nameC}
+          </td>
+          {compResult.reverse().map((comp, mostRecentIndex) => {
+            console.log("comp", comp);
+            return (
+              <td className="result" key={mostRecentIndex}>
+                {comp.resultValues[index].value}
+              </td>
+            );
+          })}
           <td className="normal-range">
             {resultDetails.analysArray[noOfAnlyze]
               ? resultDetails.analysArray[noOfAnlyze].compnents[index]
@@ -44,8 +79,11 @@ export default function ResultsTable({ darkMode, resultDetails }) {
             >
               <thead class="thead-light">
                 <tr>
-                  <th></th>
-                  <th>New Result</th>
+                  <th className="comp-name"></th>
+                  {renderNoOfResults(
+                    anlyze.result[0].compontResult.length,
+                    anlyze.result[0].compontResult
+                  )}
                   <th className="normal-range">Normal Range</th>
                   <th>Unit</th>
                 </tr>
@@ -59,6 +97,7 @@ export default function ResultsTable({ darkMode, resultDetails }) {
       return <div>Loding....</div>;
     }
   }
+  ///////////
   useEffect(() => {
     console.log("rr: ", resultDetails);
   }, []);
---
+++ client/src/methods/FormateDate.js
@@ -0,0 +1,41 @@
+//formate Date
+export const formatDate = (inputDate) => {
+  const date = new Date(inputDate);
+
+  // Format date components
+  const day = date.getDate();
+  const month = date.getMonth() + 1;
+  const year = date.getFullYear(); // Get last two digits of the year
+  const hours = date.getHours();
+  const minutes = date.getMinutes();
+  const ampm = hours >= 12 ? "PM" : "AM";
+
+  // Format with leading zeros
+  const formattedDay = day < 10 ? `0${day}` : day;
+  const formattedMonth = month < 10 ? `0${month}` : month;
+  const formattedYear = year;
+  const formattedHours = hours % 12 || 12; // Convert to 12-hour format
+  const formattedMinutes = minutes < 10 ? `0${minutes}` : minutes;
+
+  // Construct the formatted date string
+  const formattedDate = `${formattedDay}-${formattedMonth}-${formattedYear}, ${formattedHours}:${formattedMinutes} ${ampm}`;
+
+  return formattedDate;
+};
+
+export const formatDateWithouHour = (inputDate) => {
+  // Create a new Date object
+  const currentDate = new Date(inputDate);
+  const daysOfWeek = ["Sat", "Sun", "Mon", "Tue", "Wed", "Thu", "Fri"];
+  // Get the current time
+
+  const year = currentDate.getFullYear();
+  const month = currentDate.getMonth() + 1;
+  const day = currentDate.getDate();
+  // Format with leading zeros
+  const formattedDay = day < 10 ? `0${day}` : day;
+  const formattedMonth = month < 10 ? `0${month}` : month;
+  const formattedYear = year;
+
+  return `${formattedDay}/${formattedMonth}/${formattedYear}`;
+};
