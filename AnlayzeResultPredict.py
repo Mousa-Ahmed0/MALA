90cec172c6f88daf541752712b9fa1d29afa3f28
OmarKH24 <omark.nasser24@gmail.com>
2023-12-25 00:08:29 +0200

    comments for Ai to help in understant

--- AnlayzeResultPredict.py
+++ AnlayzeResultPredict.py
@@ -7,14 +7,12 @@ import numpy as np
 ###########################################
 print("---Start of Program---")
 
+#get Data from .csv file
 dataSet = pd.read_csv("CBC data_for_meandeley_csv.csv")
-print(f"hh",dataSet.head(3))
-##
+print(dataSet.head(3)) #print to sure that the data is true
+#
 
-
-
-
-# E.g dataset
+# E.g dataset to test our first model
 data = {
     "patient_Ident": ["P1", "P2", "P3", "P4", "P5"],
     "doctor_Ident": ["D1", "D2", "D3", "D4", "D5"],
@@ -29,6 +27,7 @@ data = {
     "predicted_issue": ["Healthy", "Unhealthy", "Healthy", "Unhealthy", "Unhealthy"],
 }
 
+#create DataFrame 'df'
 df = pd.DataFrame(data)
 
 # Extract 'component_results' and create columns for 'C1_resultValue' and 'C2_resultValue'
@@ -46,7 +45,8 @@ print("_______________DF X:__________________")
 print(X)
 
 # create numarical labels
-label_encoders = {}
+label_encoders = {} #save the label encoders in dictinory as ' label_encoder[column] = label_encoder ' to handale new labels in new data
+# for on all our df columns
 for column in ['patient_Ident', 'doctor_Ident', 'date', 'C1_resultValue', 'C2_resultValue']:
     label_encoder = LabelEncoder()
     X[column] = label_encoder.fit_transform(X[column])
@@ -55,7 +55,7 @@ for column in ['patient_Ident', 'doctor_Ident', 'date', 'C1_resultValue', 'C2_re
 print("________________DF X after converted to numercal:_________________")
 print(X)
 
-# convert ' predicted_issue ' to numarical
+# convert ' predicted_issue ' to numarical using LabelEncoder
 label_encoder_issue = LabelEncoder()
 y = label_encoder_issue.fit_transform(df['predicted_issue'])
 
@@ -64,10 +64,10 @@ print("--- Predicted Issue y: ---")
 print(y)
 
 # Split the data to training and testing sets
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42) # 20% of the data  for testing, and 80%  for training.
 
 # Initialize the RandomForestClassifier
-clf = RandomForestClassifier(random_state=42, n_estimators=20)
+clf = RandomForestClassifier(random_state=42, n_estimators=20) # RandomForestClassifier algorithm
 
 # Train the model
 clf.fit(X_train, y_train)
@@ -124,16 +124,17 @@ print(newResult)
 #
 
 # Check for new categories in each column
-for column in newResult.columns:
-    if column in label_encoders:
-        new_categories = set(newResult[column]) - set(label_encoders[column].classes_)
+for column in newResult.columns: # newResult dataFrame columns
+    if column in label_encoders: # cheack if coumn in our label_encoders dictionry
+        new_categories = set(newResult[column]) - set(label_encoders[column].classes_) #detrmine if new or not
         if new_categories:
             # Add new categories to the label encoder classes
             label_encoders[column].classes_ = np.concatenate([label_encoders[column].classes_, list(new_categories)])
             # Transform the new data for the current column
             newResult[column] = label_encoders[column].transform(newResult[column])
 
-        else: newResult[column] = label_encoder.transform(newResult[column])
+        #else the category is already exist => transform it
+        else: newResult[column] = label_encoder.transform(newResult[column]) 
 
 
 ###
