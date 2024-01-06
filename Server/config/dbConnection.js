4fec78d582e91743f1df93b90206f5dec8e8585a
Mousa-Ahmed0 <mousa.progm@gmail.com>
2024-01-06 10:42:27 +0200

    database

--- Server/config/dbConnection.js
+++ Server/config/dbConnection.js
@@ -1,9 +1,10 @@
 const mongoose = require("mongoose");
 module.exports = async () => {
   try {
-    await mongoose.connect("mongodb://0.0.0.0:27017/MALM");
+    // await mongoose.connect("mongodb://0.0.0.0:27017/MALM");
+    await mongoose.connect("mongodb+srv://mousa:4lA0Oc18IGzJc8w0@cluster0.ixgeo.mongodb.net/MALM");
     
-    console.log("connected to database");
+    console.log("connected to database"); 
   } catch (error) {
     console.log("connected failde to database", error);
   }
