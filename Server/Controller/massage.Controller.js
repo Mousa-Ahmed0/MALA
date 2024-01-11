10b541e859daebbe775f6a0411d61a43fdf9b8ed
Mousa-Ahmed0 <mousa.progm@gmail.com>
2024-01-11 16:10:18 +0200

    try/catch

--- Server/Controller/massage.Controller.js
+++ Server/Controller/massage.Controller.js
@@ -18,7 +18,16 @@ const socketIO = require("socket.io");
 //     console.log('User disconnected');
 //   });
 // });
+const express = require('express');
+const http = require('http');
+const socketio = require('socket.io');
 
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+server.listen(3005, () => {
+  console.log('WebSocket server is running on port 3005');
+});
 /**--------------------------------
  * @desc Send Massage
  * @router /api/massage/sendMassage
@@ -27,11 +36,8 @@ const socketIO = require("socket.io");
  * ------------------------------------------ */
 module.exports.sendMass = asyncHandler(async (req, res) => {
   try {
-
-    //if Patient must be determain admin _id
     let objectIdString = "";
     if (req.user.usertype === "Patient") objectIdString = process.env.ADMIN_ID;
-    //admin _id - 659928039f6a2dee27595dcc
     else objectIdString = req.body.secondUser;
 
     let massRecord = await Massage.findOne({
@@ -41,46 +47,43 @@ module.exports.sendMass = asyncHandler(async (req, res) => {
       ],
     });
 
+    const io = socketio(); // Replace with your actual socket.io initialization
+
     if (massRecord) {
-      // Massage record exists, update it
       const indx = massRecord.massage;
       const last = indx[indx.length - 1];
 
-      // const lastId=last.senderId.toString();
-
       if (req.user.id == last.senderId) {
-        //last measage from same user
         if (req.user.id == massRecord.firstUser) {
-          //firstUser
           massRecord.ifReadyFirstUser = true;
           massRecord.ifReadySecondUser = false;
         } else if (req.user.id == massRecord.secondUser) {
-          //secondUser
           massRecord.ifReadyFirstUser = false;
           massRecord.ifReadySecondUser = true;
         }
       } else {
-        //last measage from another user
         if (req.user.id == massRecord.firstUser) {
-          //firstUser
           massRecord.ifReadyFirstUser = true;
           massRecord.ifReadySecondUser = false;
         } else if (req.user.id == massRecord.secondUser) {
-          //secondUser
           massRecord.ifReadyFirstUser = false;
           massRecord.ifReadySecondUser = true;
         }
       }
+
       massRecord.massage.push({
         senderId: req.user.id,
         mass: req.body.massage,
         date: new Date(),
       });
+
       await massRecord.save();
-      // io.emit('message', massRecord); // Emit a message to all connected clients
+
+      // Emit a message event to the relevant room (based on the users' IDs)
+      io.to(req.user.id).to(objectIdString).emit('message', massRecord);
+
       return res.status(200).json(massRecord);
     } else {
-      // First massage
       const newMass = new Massage({
         firstUser: req.user.id,
         secondUser: objectIdString,
@@ -94,15 +97,20 @@ module.exports.sendMass = asyncHandler(async (req, res) => {
         ifReadyFirstUser: true,
         ifReadySecondUser: false,
       });
+
       await newMass.save();
-      // io.emit('message', newMass); // Emit a message to all connected clients
-      console.log("message", newMass);
+
+      // Create a room for the two users and join them
+      io.join(req.user.id);
+      io.join(objectIdString);
+
+      // Emit a message event to the relevant room
+      io.to(req.user.id).to(objectIdString).emit('message', newMass);
 
       return res.status(200).json(newMass);
     }
   } catch (error) {
-    // Handle the error here, you can log it or send a specific error response to the client
-    console.error("Error sending email:", error);
+    console.error("Error sending message:", error);
     res.status(500).json({ error: "Internal Server Error" });
   }
 });
